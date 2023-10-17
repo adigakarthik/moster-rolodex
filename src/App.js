2a0c4b3125352d6445a365ce3ecd782d0a5a9ebb
Karthik Vadhiraj <karthik.vadhiraj@ccmllc.com>
2023-10-17 17:45:03 -0500

    initial code check-in | states & secondary callback

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      name: {firstName:'Guest', lastName:''},
+      company : 'ZTM'
+    };
+  }
+  onClickEvent = () =>{
+    
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName} , I work at {this.state.company}
+          </p>
+          <button onClick={()=>{
+            this.setState(()=>{
+              return {
+                name: {firstName: 'Karthik', lastName: 'Adiga'}
+              }
+            }
+            , ()=>{
+              console.log('inside cb', this.state)
+            }
+            );
+            // console.log('outside', this.state)
+          }}>Change Name</button>
+        </header>
+      </div>
+    );
+  }  
+}
+
+export default App;
