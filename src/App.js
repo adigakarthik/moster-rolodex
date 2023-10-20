c50160794b7f68e2c92dc84de29236bc169433f9
Karthik Vadhiraj <karthik.vadhiraj@ccmllc.com>
2023-10-20 09:57:35 -0500

    react hooks useEffect & useState inside app.js

--- src/App.js
+++ src/App.js
@@ -1,75 +1,53 @@
-import { Component } from "react";
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([monsters]);
+  // console.log('render',{ searchField });
 
-  // this is used to load the api data while component is created very first time
-  // good place to do network requests
-  componentDidMount() {
-    this.loadData();
-  }
-
-  //Call the 3rd party api data
-  loadData = () => {
+  //here it is only called during the mount phase
+  //as the it has empty array []
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            // this is a callback once state is set
-            console.log("Api data loaded to state");
-          }
-        )
-      );
-  };
+      .then((users) => setMonsters(users));
+  }, []);
+
+  // here filterd monsters are trigerred only if search field/ monsters data changes
+  useEffect(() => {
+    // console.log(`inside useEffect for setFilteredMonsters`);
+    const newFilteredMonsters = monsters.filter((m) => {
+      return m.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   // create a named fn for optimization
-  // this a callback via searchbox & updates the state
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    console.log(searchField);
+  // this a callback via searchbox
+  const onSearchChange = (event) => {
+    const searchFieldValue = event.target.value.toLocaleLowerCase();
+    // console.log(`inside onSearchChange ${searchFieldValue}`);
 
     // set & trigger render
     // here we're just updating the search text & let render handle the UI update
-    this.setState(() => {
-      return { searchField };
-    });
+    setSearchField(searchFieldValue);
   };
 
-  render() {
-    // casting variables for readability
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
+  return (
+    <div className="App">
+      <h1 className="app-title">Monster Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder="find monsters"
+        className="search-box"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
-    // this is to leverage the filter than the orig data set
-    const filteredMonsters = monsters.filter((m) => {
-      return m.name.toLocaleLowerCase().includes(searchField);
-    });
-
-    return (
-      <div className="App">
-        <h1 className="app-title">Monster Rolodex</h1>
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          placeholder="find monsters"
-          className="search-box"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
 export default App;
